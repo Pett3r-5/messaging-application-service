fbe725c5d50ddcf570bc7d3aac8b6b17b8b865fc
Edison_Petter <Edison_Petter@Dell.com>
2021-04-28 23:56:47 -0300

    fixing cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(parseInt(process.env.PORT, 10) || 6000);
-  console.log(parseInt(process.env.PORT, 10) || 6000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(parseInt(process.env.PORT, 10) || 7011);
+  console.log(parseInt(process.env.PORT, 10) || 7011);
   
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+
 }
 bootstrap();
