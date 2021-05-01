9a94c4e44747d1251ad5d0de6c30daa35445ab78
Edison_Petter <Edison_Petter@Dell.com>
2021-05-01 01:17:10 -0300

    setting up env variable for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,7 @@ import { MessageModule } from './message/message.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/test'), ConversatioModule, MessageModule, UserModule ],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DATABASE_URL), ConversatioModule, MessageModule, UserModule ],
   controllers: [AppController],
   providers: [AppService],
 })
